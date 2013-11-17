4cbd4fb3c6a955dbf8c98c4bb02f0e916ba66bb6
Christian Dullweber <xchrdw@gmail.com>
2013-11-17 21:45:16 +0100

    fix linux build

--- src/utils/vec3helper.h
+++ src/utils/vec3helper.h
@@ -13,10 +13,12 @@ bool operator>=(const glm::vec3 &a, const glm::vec3 &b);
 
 bool operator<=(const glm::vec3 &a, const glm::vec3 &b);
 
-template<>
-struct snowhouse::Stringizer<glm::vec3>
-{
-    static std::string ToString(const glm::vec3& value) {
-        return toString(value);
-    }
-};
+namespace snowhouse {
+    template<>
+    struct snowhouse::Stringizer<glm::vec3>
+    {
+        static std::string ToString(const glm::vec3& value) {
+            return toString(value);
+        }
+    };
+}
