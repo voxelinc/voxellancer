c6e251a7e541f7ee34b2e57e20a9fbfb82afad1e
psieg <padi@padimail.de>
2013-11-19 02:12:17 +0100

    Add a framerate display :)

--- src/ui/hud.cpp
+++ src/ui/hud.cpp
@@ -11,13 +11,15 @@ m_lastgamecamera(),
 m_ships(ships),
 m_shiparrow(),
 m_delta_sec_remain(0),
+m_framerate(0),
 m_distance("hud.distance", 100.f),
 m_move_multiplier("hud.move_multiplier", 5.f),
 m_inertia_rotate("hud.inertia_rotate", 30.f),
 m_inertia_move("hud.inertia_move", 25.f),
 m_inertia_rate("hud.inertia_rate", .0005f),
 m_arrow_maxdistance("hud.arrow_maxdistance", 1000.f),
-m_arrow_radius("hud.arrow_radius", .7f)
+m_arrow_radius("hud.arrow_radius", .7f),
+m_show_framerate("hud.show_framerate", true)
 {
 	m_voxelRenderer = std::unique_ptr<VoxelRenderer>(new VoxelRenderer());
 
@@ -26,12 +28,23 @@ m_arrow_radius("hud.arrow_radius", .7f)
 	m_rendercamera.setZFar(1000.0f);
 	
 	ClusterLoader loader;
-	addElement(&loader, "data/hud/crosshair.csv", HUDOffsetOrigin::Center, glm::vec3(-4, -4, 0));
-	addElement(&loader, "data/hud/topleft.csv", HUDOffsetOrigin::TopLeft, glm::vec3(1, -2, 0));
-	addElement(&loader, "data/hud/topright.csv", HUDOffsetOrigin::TopRight, glm::vec3(-4, -2, 0));
-	addElement(&loader, "data/hud/bottomleft.csv", HUDOffsetOrigin::BottomLeft, glm::vec3(1, 1, 0));
-	addElement(&loader, "data/hud/bottomright.csv", HUDOffsetOrigin::BottomRight, glm::vec3(-4, 1, 0));
-	addElement(&loader, "data/hud/bottom.csv", HUDOffsetOrigin::Bottom, glm::vec3(-27, 1, 0));
+	addElement(&loader, "data/hud/crosshair.csv", HUDOffsetOrigin::Center, glm::vec3(-4, -4, 0), &m_elements);
+	addElement(&loader, "data/hud/topleft.csv", HUDOffsetOrigin::TopLeft, glm::vec3(1, -2, 0), &m_elements);
+	addElement(&loader, "data/hud/topright.csv", HUDOffsetOrigin::TopRight, glm::vec3(-4, -2, 0), &m_elements);
+	addElement(&loader, "data/hud/bottomleft.csv", HUDOffsetOrigin::BottomLeft, glm::vec3(1, 1, 0), &m_elements);
+	addElement(&loader, "data/hud/bottomright.csv", HUDOffsetOrigin::BottomRight, glm::vec3(-4, 1, 0), &m_elements);
+	addElement(&loader, "data/hud/bottom.csv", HUDOffsetOrigin::Bottom, glm::vec3(-27, 1, 0), &m_elements);
+
+	addElement(&loader, "data/hud/font/0.csv", HUDOffsetOrigin::TopLeft, glm::vec3(0, -4, 0), &m_numbers);
+	addElement(&loader, "data/hud/font/1.csv", HUDOffsetOrigin::TopLeft, glm::vec3(0, -4, 0), &m_numbers);
+	addElement(&loader, "data/hud/font/2.csv", HUDOffsetOrigin::TopLeft, glm::vec3(0, -4, 0), &m_numbers);
+	addElement(&loader, "data/hud/font/3.csv", HUDOffsetOrigin::TopLeft, glm::vec3(0, -4, 0), &m_numbers);
+	addElement(&loader, "data/hud/font/4.csv", HUDOffsetOrigin::TopLeft, glm::vec3(0, -4, 0), &m_numbers);
+	addElement(&loader, "data/hud/font/5.csv", HUDOffsetOrigin::TopLeft, glm::vec3(0, -4, 0), &m_numbers);
+	addElement(&loader, "data/hud/font/6.csv", HUDOffsetOrigin::TopLeft, glm::vec3(0, -4, 0), &m_numbers);
+	addElement(&loader, "data/hud/font/7.csv", HUDOffsetOrigin::TopLeft, glm::vec3(0, -4, 0), &m_numbers);
+	addElement(&loader, "data/hud/font/8.csv", HUDOffsetOrigin::TopLeft, glm::vec3(0, -4, 0), &m_numbers);
+	addElement(&loader, "data/hud/font/9.csv", HUDOffsetOrigin::TopLeft, glm::vec3(0, -4, 0), &m_numbers);
 
 	m_shiparrow.reset(new HUDElement);
 	loader.loadClusterFromFile("data/hud/arrow.csv", m_shiparrow.get());
@@ -40,12 +53,13 @@ m_arrow_radius("hud.arrow_radius", .7f)
 
 }
 
-void HUD::addElement(ClusterLoader *loader, char* filename, HUDOffsetOrigin origin, glm::vec3 offset){
+
+void HUD::addElement(ClusterLoader *loader, char* filename, HUDOffsetOrigin origin, glm::vec3 offset, std::vector<std::unique_ptr<HUDElement>> *list){
 	std::unique_ptr<HUDElement> element(new HUDElement);
 	loader->loadClusterFromFile(filename, element.get());
 	element->m_origin = origin;
 	element->m_offset = offset;
-	m_elements.push_back(move(element));
+	list->push_back(move(element));
 }
 
 HUD::~HUD(){
@@ -77,8 +91,10 @@ void HUD::update(float delta_sec){
 	m_delta_sec_remain = delta_sec - progress;
 	m_lastgamecamera.setOrientation(m_gamecamera->orientation());
 	m_lastgamecamera.setPosition(m_gamecamera->position());
-	//m_hudcamera.setOrientation(glm::mix(m_hudcamera.orientation() , m_gamecamera->orientation(), glm::min(delta_sec * m_inertia_rotate, 1.0f)));
-	//m_hudcamera.setPosition(glm::mix(m_hudcamera.position(), m_gamecamera->position(), glm::min(delta_sec * m_inertia_move, 1.0f)));
+
+	float thisframe = 1.f / delta_sec;
+	if (thisframe < 1 || thisframe > 9999) m_framerate = 0;
+	else m_framerate = m_framerate * .8f + thisframe * .2f;
 }
 
 void HUD::draw(){
@@ -92,6 +108,9 @@ void HUD::draw(){
 	m_rendercamera.setOrientation((m_gamecamera->orientation()*glm::inverse(m_hudcamera.orientation())));
 	m_rendercamera.setPosition(m_hudcamera.orientation() * ((m_gamecamera->position() - m_hudcamera.position()) * m_move_multiplier.get()));
 
+	float dy = floor(glm::tan(glm::radians(m_rendercamera.fovy() / 2)) * m_distance);
+	float dx = m_rendercamera.aspectRatio()*dy;
+
 	m_voxelRenderer->prepareDraw(&m_rendercamera, false);
 
 	// draw statics
@@ -100,7 +119,6 @@ void HUD::draw(){
 	}
 
 	// draw ship arrows
-	float dy = floor(glm::tan(glm::radians(m_rendercamera.fovy() / 2)) * m_distance);
 	for (VoxelCluster *ship : m_ships){
 		if (glm::length(ship->worldTransform().position() - m_hudcamera.position()) < m_arrow_maxdistance){
 			// delta is the vector from virtual HUD camera to the ship
@@ -128,6 +146,22 @@ void HUD::draw(){
 				m_voxelRenderer->draw(m_shiparrow.get());
 			}
 		}
+	}
+
+	// draw frame rate
+	if (m_show_framerate){
+		int i1, i2;
+		if (m_framerate > 99.f){ i1 = 9; i2 = 9; }
+		else {
+			i1 = (int) glm::floor(m_framerate / 10.f);
+			i2 = (int) m_framerate % 10;
+		}
+		HUDElement *num = m_numbers[i1].get();
+		num->transform(-num->worldTransform().position() + glm::vec3(-dx + 3, dy - 3, -m_distance) + num->m_offset);
+		m_voxelRenderer->draw(num);
+		num = m_numbers[i2].get();
+		num->transform(-num->worldTransform().position() + glm::vec3(-dx + 8, dy - 3, -m_distance) + num->m_offset);
+		m_voxelRenderer->draw(num);
 	}
 
 	m_voxelRenderer->afterDraw();
--- src/ui/hud.h
+++ src/ui/hud.h
@@ -25,17 +25,20 @@ public:
 
 protected:
 	void adjustPositions();
-	void addElement(ClusterLoader *loader, char *filename, HUDOffsetOrigin origin, glm::vec3 offset);
+	void addElement(ClusterLoader *loader, char *filename, HUDOffsetOrigin origin, glm::vec3 offset, std::vector<std::unique_ptr<HUDElement>> *list);
 	void stepAnim(glm::vec3 targetpos, glm::quat targetor);
 
 	Camera *m_gamecamera;
 	WorldTransform m_hudcamera, m_lastgamecamera;
 	Camera m_rendercamera;
 	std::unique_ptr<VoxelRenderer> m_voxelRenderer;
-	std::list<std::unique_ptr<HUDElement>> m_elements;
+	std::vector<std::unique_ptr<HUDElement>> m_elements;
+	std::vector<std::unique_ptr<HUDElement>> m_numbers;
 	std::unique_ptr<HUDElement> m_shiparrow;
 	std::list<VoxelCluster*> m_ships;
 	float m_delta_sec_remain;
+	float m_framerate;
 
 	Property<float> m_distance, m_move_multiplier, m_inertia_move, m_inertia_rotate, m_inertia_rate, m_arrow_maxdistance, m_arrow_radius;
+	Property<bool> m_show_framerate;
 };
