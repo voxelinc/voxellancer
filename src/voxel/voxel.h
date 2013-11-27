e0cf50af67eb4352188ae051971ab808d393ccaf
Christian Dullweber <xchrdw@gmail.com>
2013-11-27 21:53:58 +0100

    fix linux build

--- src/voxel/voxel.h
+++ src/voxel/voxel.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <functional>
+
 #include <glm/glm.hpp>
 #include "geometry/sphere.h"
 
@@ -7,9 +9,9 @@
 typedef glm::detail::tvec3<unsigned char> cvec3;
  
 namespace std {
-    template <> struct hash<cvec3>
+    template<> struct hash<cvec3>
     {
-        std::size_t operator()(const cvec3& v) const {
+        std::size_t operator()(const cvec3 &v) const {
             return v.x + (v.y << 8) + (v.z << 16);
         }
     };
@@ -24,7 +26,7 @@ public:
     Voxel(cvec3 gridCell, cvec3 color);
     virtual ~Voxel();
 
-    // TODO: extract to PhysicalVoxel
+    // TODO: extract to WorldCluster
     VoxelCluster *voxelCluster();
     void setVoxelCluster(VoxelCluster *cluster);
 
