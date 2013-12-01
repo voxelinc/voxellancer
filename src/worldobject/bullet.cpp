7aec8d7f5d78bff67bdb1e63a3a17232b15c19a0
psieg <padi@padimail.de>
2013-12-01 22:32:04 +0100

    Fix orientation when ship not-aligned

--- src/worldobject/bullet.cpp
+++ src/worldobject/bullet.cpp
@@ -3,13 +3,13 @@
 Bullet::Bullet(glm::vec3 position, glm::quat orientation, glm::vec3 direction, float speed, float range){
     m_range = range / speed;
     glm::vec3 dir = glm::normalize(direction);
-    glm::vec3 myOrientation = glm::inverse(orientation)*glm::vec3(0, 0, -1);
+    glm::vec3 myOrientation = orientation * glm::vec3(0, 0, -1);
     glm::vec3 rotationAxis = glm::normalize(glm::cross(dir, myOrientation));
     float angle = glm::acos(glm::dot(dir, myOrientation));
 
     m_transform.setPosition(position);
     m_transform.setOrientation(orientation); //set orientation to ship orientation
-    m_transform.rotate(glm::angleAxis(-glm::degrees(angle), rotationAxis));
+    m_transform.rotateWorld(glm::angleAxis(-glm::degrees(angle), rotationAxis));
     setSpeed(dir*speed);
 }
 
