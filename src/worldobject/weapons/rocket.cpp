c9c56a75fc0c755996cbca5ff153e21fede43f68
psieg <padi@padimail.de>
2013-12-07 23:14:29 +0100

    Make rockets fly straight xD

--- src/worldobject/weapons/rocket.cpp
+++ src/worldobject/weapons/rocket.cpp
@@ -14,9 +14,9 @@ Rocket::Rocket(glm::vec3 position, glm::quat orientation, const glm::vec3& initi
     m_travelSpeed = travelSpeed;
     glm::vec3 myOrientation = orientation * glm::vec3(0, 0, -1);
 
-    ClusterCache::instance()->fillObject(this, "data/voxelcluster/rocket.csv");
+        ClusterCache::instance()->fillObject(this, "data/voxelcluster/rocket.csv");
 
-    m_transform.setOrientation(orientation); //set orientation to ship orientation
+        m_transform.setOrientation(orientation); //set orientation to ship orientation
 
 
     m_transform.setPosition(position + myOrientation * (m_collisionDetector->voxeltree().gridAABB().axisMax(Axis::ZAxis) / 2.0f + 1.4f));
@@ -32,13 +32,13 @@ Rocket::Rocket(glm::vec3 position, glm::quat orientation, const glm::vec3& initi
 
 void Rocket::update(float delta_sec){
     // orient towards target
-
+    
 
     // accelerate to travelSpeed
     if (glm::length(m_physics->speed()) < m_travelSpeed){
         float missingSpeed = m_travelSpeed - glm::length(m_physics->speed());
         // accelerate forward, not towards target
-        m_physics->accelerate(m_transform.orientation() * glm::vec3(0, 0, -missingSpeed));
+        m_physics->accelerate(glm::vec3(0, 0, -missingSpeed));
     }
 
     m_lifetime -= delta_sec;
