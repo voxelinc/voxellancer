347c35a524d2e703964af5aa3af0501745191c89
mrzzzrm <mrzesn@t-online.de>
2013-12-14 00:07:19 +0100

    added missing files for collision_filter

---
+++ src/collision/collisionfilterable.cpp
@@ -0,0 +1,39 @@
+#include "collisionfilterable.h"
+
+CollisionFilterable::CollisionFilterable(CollisionFilterClass collisionFilterClass, uint32_t collisionMask):
+    m_collisionFilterClass(collisionFilterClass),
+    m_collisionMask(collisionMask)
+{
+}
+
+CollisionFilterClass CollisionFilterable::collisionFilterClass() const {
+    return m_collisionFilterClass;
+}
+
+uint32_t CollisionFilterable::collisionMask() const {
+    return m_collisionMask;
+}
+
+void CollisionFilterable::setCollideableWith(CollisionFilterClass collisionFilterClass, bool collides) {
+    if(collides) {
+        m_collisionMask |= static_cast<uint32_t>(collisionFilterClass);
+    }
+    else {
+        m_collisionMask &= ~static_cast<uint32_t>(collisionFilterClass);
+    }
+}
+
+bool CollisionFilterable::isCollideableWith(const CollisionFilterable *other) const {
+    return  (this != other) && areMasksCollidable(other) && specialIsCollideableWith(other) && other->specialIsCollideableWith(this);
+
+}
+
+bool CollisionFilterable::areMasksCollidable(const CollisionFilterable *other) const {
+    return (static_cast<uint32_t>(m_collisionFilterClass) | other->collisionMask()) &&
+           (static_cast<uint32_t>(other->collisionFilterClass()) | m_collisionMask);
+}
+
+bool CollisionFilterable::specialIsCollideableWith(const CollisionFilterable *other) const {
+    return true;
+}
+
---
+++ src/collision/collisionfilterclass.h
@@ -0,0 +1,10 @@
+#pragma once
+
+
+enum class CollisionFilterClass {
+    Bullet,
+    Rocket,
+    Ship,
+    Other
+};
+
