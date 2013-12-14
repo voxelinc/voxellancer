ff224869dcae919aa5d9f558182b4fbed5035d1e
psieg <padi@padimail.de>
2013-12-14 01:37:06 +0100

    Re-enable hooks and comment out problematic tostring stuff

--- src/utils/tostring.h
+++ src/utils/tostring.h
@@ -14,7 +14,7 @@ std::string toString(const glm::detail::tvec3<T>& v);
 template<typename T>
 std::string toString(const TAABB<T>& aabb);
 
-std::string toString(const Sphere& sphere);
+//std::string toString(const Sphere& sphere);
 
 
 #include "tostring.inl"
--- src/worldobject/weapons/bullet.h
+++ src/worldobject/weapons/bullet.h
@@ -7,8 +7,8 @@ public:
     Bullet(glm::vec3 position, glm::quat orientation, glm::vec3 direction, float speed, float range);
     virtual void update(float delta_sec);
 
-    virtual void onImpact();
-    virtual void onSpawnFail();
+    virtual void onCollision() override;
+    virtual void onSpawnFail() override;
 
 private:
 	float m_lifetime;
