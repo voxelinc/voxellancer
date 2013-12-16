98fbb1ef9cc61b89d37f79fa12e584bea44c0d0d
psieg <padi@padimail.de>
2013-12-16 22:19:37 +0100

    input cleaup

--- src/ui/inputhandler.cpp
+++ src/ui/inputhandler.cpp
@@ -27,32 +27,12 @@
 * B9: right stick
 */
 
-InputHandler::InputHandler(GLFWwindow *window, Camera *camera) :
-m_window(window),
-m_camera(camera),
-prop_deadzone("input.deadzone"),
-m_followCam(true)
-{
-
-    bumperLeftState = false;
-    bumperRightState = false;
-    glfwGetWindowSize(m_window, &m_windowWidth, &m_windowHeight);
-    glfwSetCursorPos(m_window, m_windowWidth / 2, m_windowHeight / 2);
-    m_cursorMaxDistance = glm::min(m_windowHeight, m_windowWidth)/2;
-
-    m_fpsControls = false;
-    m_mouseControl = false;
-    m_lastfocus = glfwGetWindowAttrib(m_window, GLFW_FOCUSED);
-
-    glfwSetInputMode(m_window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
-}
 
 InputHandler::InputHandler(GLFWwindow *window, Player* player, Camera *camera) :
-m_window(window),
-m_player(player),
-m_camera(camera),
-prop_deadzone("input.deadzone"),
-m_followCam(true)
+    m_window(window),
+    m_player(player),
+    m_camera(camera),
+    prop_deadzone("input.deadzone")
 {
     bumperLeftState = false;
     bumperRightState = false;
@@ -60,7 +40,6 @@ m_followCam(true)
     glfwSetCursorPos(m_window, m_windowWidth / 2, m_windowHeight / 2);
     m_cursorMaxDistance = glm::min(m_windowHeight, m_windowWidth)/2;
 
-    m_fpsControls = false;
     m_mouseControl = false;
     m_lastfocus = glfwGetWindowAttrib(m_window, GLFW_FOCUSED);
 
@@ -72,10 +51,7 @@ InputHandler::~InputHandler(){
 }
 
 
-void InputHandler::resizeEvent(
-	const unsigned int width
-	, const unsigned int height)
-{
+void InputHandler::resizeEvent(const unsigned int width, const unsigned int height){
 	//glfwGetWindowSize(m_window, &m_windowWidth, &m_windowHeight);
 	m_windowWidth = width;
 	m_windowHeight = height;
@@ -85,14 +61,10 @@ void InputHandler::resizeEvent(
 
 void InputHandler::keyCallback(int key, int scancode, int action, int mods){
 	/* Check here for single-time key-presses, that you do not want fired multiple times, e.g. toggles */
-	if (key == GLFW_KEY_F1 && action == GLFW_PRESS)
-		toggleControls();
 	if (key == GLFW_KEY_SPACE && action == GLFW_PRESS)
 		m_mouseControl = !m_mouseControl;
-    if (key == GLFW_KEY_M && action == GLFW_PRESS)
-        m_followCam = !m_followCam;
     if (key == GLFW_KEY_TAB && action == GLFW_PRESS)
-        selectNextTarget(true);
+        selectNextTarget(glfwGetKey(m_window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS);
 }
 
 void InputHandler::update(float delta_sec) {
@@ -136,12 +108,7 @@ void InputHandler::update(float delta_sec) {
                 const unsigned char *buttons = glfwGetJoystickButtons(GLFW_JOYSTICK_1, &cnt);
                 const float *axes = glfwGetJoystickAxes(GLFW_JOYSTICK_1, &cntAxes);
 
-                for (int i = 0; i < cnt; ++i) {
-                    if (buttons[i] == GLFW_PRESS) {
-                        glow::debug("button: %; pressed", i);
-                    }
 
-                }
                 if (buttons[4] == GLFW_PRESS){
                     if (!bumperLeftState)
                         this->selectNextTarget(false);
@@ -181,12 +148,8 @@ void InputHandler::update(float delta_sec) {
                     rot = glm::normalize(rot);
                 }
                 m_player->rotate(rot);
-                glow::debug("%; \n ", glfwJoystickPresent(GLFW_JOYSTICK_1));
 
             }
-
-
-
 
 
             // mouse handling
@@ -207,60 +170,28 @@ void InputHandler::update(float delta_sec) {
             float angX = 0;
             float angY = 0;
 
-			if (!m_fpsControls) {
-				if (m_mouseControl || glfwGetMouseButton(m_window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS){
-                    glm::vec3 rot;
-                    x = m_windowWidth / 2 - (int)floor(x);
-                    y = m_windowHeight / 2 - (int)floor(y);
-                    x = glm::min((double)m_cursorMaxDistance, x);
-                    y = glm::min((double)m_cursorMaxDistance, y);
-                    rot = glm::vec3(y,x, 0);
-                    rot /= m_cursorMaxDistance;
-                    if (glm::length(rot) < prop_deadzone)
-                        rot = glm::vec3(0);
-                    if (glm::length(rot) > 1){
-                        rot = glm::normalize(rot);
-                    }
-                    m_player->rotate(rot);
-				}
-                else {
-                    rel = 0;
+            if (m_mouseControl || glfwGetMouseButton(m_window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS){
+                glm::vec3 rot;
+                x = m_windowWidth / 2 - (int)floor(x);
+                y = m_windowHeight / 2 - (int)floor(y);
+                x = glm::min((double)m_cursorMaxDistance, x);
+                y = glm::min((double)m_cursorMaxDistance, y);
+                rot = glm::vec3(y, x, 0);
+                rot /= m_cursorMaxDistance;
+                if (glm::length(rot) < prop_deadzone)
+                    rot = glm::vec3(0);
+                if (glm::length(rot) > 1){
+                    rot = glm::normalize(rot);
                 }
-			}
-            else {
-                angX = ((int)floor(x) - m_windowWidth / 2) * rel;
-                angY = ((int)floor(y) - m_windowHeight / 2) * rel;
+                m_player->rotate(rot);
             }
-
 		}
-
-
-        if (m_fpsControls)
-            glfwSetCursorPos(m_window, m_windowWidth / 2, m_windowHeight / 2);
-
     }
 
-    if (m_followCam){
-        m_player->applyAcceleration();
-    }
-    else{
-        m_player->setShipToCam(delta_sec);
-    }
+    m_player->applyAcceleration();
 
 	m_lastfocus = glfwGetWindowAttrib(m_window, GLFW_FOCUSED);
 }
-
-void InputHandler::toggleControls()
-{
-	if (m_fpsControls) {
-		glfwSetInputMode(m_window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
-	}
-	else {
-		glfwSetInputMode(m_window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
-	}
-	m_fpsControls = !m_fpsControls;
-}
-
 
 WorldObject* InputHandler::findNextTarget(bool forward){
     std::list<WorldObject*>& worldObjects = World::instance()->worldObjects();
@@ -324,14 +255,6 @@ WorldObject* InputHandler::findNextTarget(bool forward){
 void InputHandler::selectNextTarget(bool forward){
     m_player->playerShip()->setTargetObject(findNextTarget(forward));
 }
-
-std::string InputHandler::playerTarget(){
-    if (m_player->playerShip()->targetObject())
-        return "Locked: " + m_player->playerShip()->targetObject()->objectInfo().name();
-    else
-        return "No Lock";
-}
-
 glm::vec3 InputHandler::findTargetPoint(double x, double y){
     glm::vec4 pointEnd((x * 2 / m_windowWidth - 1), -1 * (y * 2 / m_windowHeight - 1), 1, 1); //get normalized device coords
     pointEnd = glm::inverse(m_camera->viewProjection())*pointEnd; //find point on zfar
