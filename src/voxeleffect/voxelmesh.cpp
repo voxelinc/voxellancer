69ba113a8ce829947f7049da94b6c382cd152acd
mrzzzrm <mrzesn@t-online.de>
2014-01-05 19:27:33 +0100

    removed duplicate voxelmesh specification

--- src/voxeleffect/voxelmesh.cpp
+++ src/voxeleffect/voxelmesh.cpp
@@ -17,29 +17,6 @@ static const glm::vec3 front(0, 0, -1);
 static const glm::vec3 back(0, 0, 1);
 static const glm::vec3 dummy(0, 0, 0) ;
 
-glm::vec3 VERTICES[8]
-{
-    glm::vec3(-.5f, -.5f, -.5f)
-        , glm::vec3(-.5f, -.5f, .5f)
-        , glm::vec3(-.5f, .5f, -.5f)
-        , glm::vec3(-.5f, .5f, .5f)
-        , glm::vec3(.5f, -.5f, -.5f)
-        , glm::vec3(.5f, -.5f, .5f)
-        , glm::vec3(.5f, .5f, -.5f)
-        , glm::vec3(.5f, .5f, .5f)
-};
-
-glm::vec3 NORMALS[7]
-{
-    glm::vec3(-1, 0, 0)
-        , glm::vec3(1, 0, 0)
-        , glm::vec3(0, -1, 0)
-        , glm::vec3(0, 1, 0)
-        , glm::vec3(0, 0, -1)
-        , glm::vec3(0, 0, 1)
-        , glm::vec3(0, 0, 0)  // dummy
-};
-
 glow::Array<glm::vec3> VoxelMesh::s_vertices
 {
     rub,
@@ -70,44 +47,11 @@ glow::Array<glm::vec3> VoxelMesh::s_normals
     front, front
 };
 
-
 const glow::Array<glm::vec3> VoxelMesh::vertices() {
-    return glow::Array<glm::vec3>
-    {
-        VERTICES[7]
-            , VERTICES[3]
-            , VERTICES[5]
-            , VERTICES[1]
-            , VERTICES[0]
-            , VERTICES[3]
-            , VERTICES[2]
-            , VERTICES[7]
-            , VERTICES[6]
-            , VERTICES[5]
-            , VERTICES[4]
-            , VERTICES[0]
-            , VERTICES[6]
-            , VERTICES[2]
-    };
+    return s_vertices;
 }
 
 const glow::Array<glm::vec3> VoxelMesh::normals() {
-    return glow::Array<glm::vec3>
-    {
-        NORMALS[6]
-            ,  NORMALS[6]
-            ,  NORMALS[5]
-            , NORMALS[5]
-            ,  NORMALS[2]
-            ,  NORMALS[0]
-            ,  NORMALS[0]
-            ,  NORMALS[3]
-            ,  NORMALS[3]
-            ,  NORMALS[1]
-            ,  NORMALS[1]
-            , NORMALS[2]
-            ,  NORMALS[4]
-            , NORMALS[4]
-    };
+    return s_normals;
 }
 
