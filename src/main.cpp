941379762a5d4d72f589133687e0772cb9ff3578
mrzzzrm <mrzesn@t-online.de>
2014-01-05 21:42:58 +0100

    switchback to GL 3.1

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@
 #include "game.h"
 #include "ui/inputhandler.h"
 
-static GLint MajorVersionRequire = 2;
+static GLint MajorVersionRequire = 3;
 static GLint MinorVersionRequire = 1;
 
 static Game * game;
