6ca46fd98a3d99fd1869b0436ede14e2e547eb5e
mrzzzrm <mrzesn@t-online.de>
2014-01-05 23:11:52 +0100

    lib-setup-script checks out specific tags/commits

--- lib/setup_libs.sh
+++ lib/setup_libs.sh
@@ -2,23 +2,21 @@
 
 git clone https://github.com/nigels-com/glew.git glew
 cd glew
+git checkout 98e09c1564f799040218b97ad541d6d061ffbb00
 make extensions
 make -j8
 cd ..
 
-git clone https://github.com/g-truc/glm.git glm
-cd glm
-git fetch origin
-git checkout 0.9.4
-cd ..
+git clone -b 0.9.4.6 https://github.com/g-truc/glm.git glm
 
-git clone https://github.com/glfw/glfw.git glfw
+git clone -b 3.0.3 https://github.com/glfw/glfw.git glfw
 cd glfw
+git 
 cmake . 
 make -j8
 cd ..
 
-git clone https://github.com/joakimkarlsson/bandit.git bandit
+git clone -b v1.1.4 https://github.com/joakimkarlsson/bandit.git bandit
 
 cd .. 
 git submodule init
