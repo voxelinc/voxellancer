1ffa5eb586b9eb603406b693aaa886748646a284
mrzzzrm <mrzesn@t-online.de>
2014-01-07 22:07:54 +0100

    removed 3 tests that didn't make sense anymore

--- test/worldobject/testworldobject.cpp
+++ test/worldobject/testworldobject.cpp
@@ -6,6 +6,7 @@
 #include "../bandit_extension/vec3helper.h"
 #include "worldobject/worldobject.h"
 #include "voxel/voxel.h"
+#include "voxel/voxeltreenode.h"
 
 
 using namespace bandit;
@@ -35,51 +36,26 @@ go_bandit([](){
             AssertThat(a.aabb().contains(glm::vec3(9.49, 0, 0)), Equals(true));
         });
 
-//        it("can adjust its center", [&]() {
-//            WorldObject *d = new WorldObject(1.0, glm::vec3(1,1,1));
-//
-//            AssertThat(d->collisionDetector().voxeltree().boundingSphere().position(), EqualsWithDelta(glm::vec3(-1, -1, -1), glm::vec3(0.01, 0.01, 0.01)));
-//        });
-//
-//        it("can adjust its center and scale", [&]() {
-//            WorldObject *d = new WorldObject(3.0f, glm::vec3(1, 1, 1));
-//
-//            AssertThat(d->collisionDetector().voxeltree().boundingSphere().radius(), EqualsWithDelta(2.5f, 0.1f));
-//            AssertThat(d->collisionDetector().voxeltree().boundingSphere().position(), EqualsWithDelta(glm::vec3(-3, -3, -3), glm::vec3(0.01, 0.01, 0.01)));
-//        });
-//
-//        it("works in rotated  worldobject", [&]() {
-//            WorldObject a;
-//            a.addVoxel(new Voxel(glm::ivec3(0, 0, 0), 0xFFFFFF));
-//            a.addVoxel(new Voxel(glm::ivec3(1, 0, 0), 0xFFFFFF));
-//            a.addVoxel(new Voxel(glm::ivec3(2, 0, 0), 0xFFFFFF));
-//            a.addVoxel(new Voxel(glm::ivec3(3, 0, 0), 0xFFFFFF));
-//            a.addVoxel(new Voxel(glm::ivec3(4, 0, 0), 0xFFFFFF));
-//
-//            a.rotate(glm::angleAxis(90.f, glm::vec3(0, 1, 0)));
-//
-//            AssertThat(a.aabb().contains(glm::vec3(0, 0, -2.49)), Equals(true));
-//        });
-//
-//        it("incremental mass and center calculation works", [&]() {
-//            WorldObject a(0.8f);
-//            WorldObject b(0.8f);
-//
-//            a.addVoxel(new Voxel(glm::ivec3(1, 0, 0), 0xFFFFFF));
-//            a.addVoxel(new Voxel(glm::ivec3(2, 0, 0), 0xFFFFFF));
-//            a.addVoxel(new Voxel(glm::ivec3(3, 0, 0), 0xFFFFFF));
-//            a.addVoxel(new Voxel(glm::ivec3(3, 1, 0), 0xFFFFFF));
-//            a.addVoxel(new Voxel(glm::ivec3(3, 2, 0), 0xFFFFFF));
-//            a.removeVoxel(a.voxel(glm::ivec3(3, 2, 0)));
-//
-//            b.addVoxel(new Voxel(glm::ivec3(1, 0, 0), 0xFFFFFF));
-//            b.addVoxel(new Voxel(glm::ivec3(2, 0, 0), 0xFFFFFF));
-//            b.addVoxel(new Voxel(glm::ivec3(3, 0, 0), 0xFFFFFF));
-//            b.addVoxel(new Voxel(glm::ivec3(3, 1, 0), 0xFFFFFF));
-//
-//            AssertThat(a.physics().mass(), Equals(b.physics().mass()));
-//            AssertThat(a.transform().center(), EqualsWithDelta(b.transform().center(), glm::vec3(0.01)));
-//        });
+
+        it("incremental mass calculation works", [&]() {
+            WorldObject a(0.8f);
+            WorldObject b(0.8f);
+
+            a.addVoxel(new Voxel(glm::ivec3(1, 0, 0), 0xFFFFFF));
+            a.addVoxel(new Voxel(glm::ivec3(2, 0, 0), 0xFFFFFF));
+            a.addVoxel(new Voxel(glm::ivec3(3, 0, 0), 0xFFFFFF));
+            a.addVoxel(new Voxel(glm::ivec3(3, 1, 0), 0xFFFFFF));
+            a.addVoxel(new Voxel(glm::ivec3(3, 2, 0), 0xFFFFFF));
+            a.removeVoxel(a.voxel(glm::ivec3(3, 2, 0)));
+
+            b.addVoxel(new Voxel(glm::ivec3(1, 0, 0), 0xFFFFFF));
+            b.addVoxel(new Voxel(glm::ivec3(2, 0, 0), 0xFFFFFF));
+            b.addVoxel(new Voxel(glm::ivec3(3, 0, 0), 0xFFFFFF));
+            b.addVoxel(new Voxel(glm::ivec3(3, 1, 0), 0xFFFFFF));
+
+            AssertThat(a.physics().mass(), Equals(b.physics().mass()));
+            AssertThat(a.transform().center(), EqualsWithDelta(b.transform().center(), glm::vec3(0.01)));
+        });
     });
 });
 
