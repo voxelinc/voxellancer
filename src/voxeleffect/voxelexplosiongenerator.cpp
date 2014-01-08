fca2716efcd52c88fc6d35926d07b012ede53d23
gersseba <sebastian.gerstenberg@student.hpi.uni-potsdam.de>
2014-01-08 14:46:35 +0100

    added .f to avoid type conversion warning

--- src/voxeleffect/voxelexplosiongenerator.cpp
+++ src/voxeleffect/voxelexplosiongenerator.cpp
@@ -102,9 +102,9 @@ void VoxelExplosionGenerator::spawn() {
                     m_force * RandFloat::rand(-10.0f, 10.0f)));
 
 
-                VoxelParticle* particle = new VoxelParticle(newObject->transform(), m_color, lifetime*0.1);
-                particle->setAngularSpeed(newObject->physics().angularSpeed(), 0.2);
-                particle->setDirectionalSpeed(newObject->physics().speed()*5.0f, 0.2);
+                VoxelParticle* particle = new VoxelParticle(newObject->transform(), m_color, lifetime*0.1f);
+                particle->setAngularSpeed(newObject->physics().angularSpeed(), 0.2f);
+                particle->setDirectionalSpeed(newObject->physics().speed()*5.0f, 0.2f);
 
                 World::instance()->voxelParticleWorld().addParticle(particle);
 
