1bbb647dbdeb00039fdda87cbc367a7e19d8c502
mrzzzrm <mrzesn@t-online.de>
2014-01-11 17:09:14 +0100

    added bullettransformhelper

---
+++ src/worldobject/weapons/bullettransformhelper.cpp
@@ -0,0 +1,51 @@
+#include "bullettransformhelper.h"
+
+#include <glm/glm.hpp>
+#include <glm/gtx/quaternion.hpp>
+
+#include "worldtransform.h"
+
+#include "worldobject/hardpoint.h"
+#include "worldobject/ship.h"
+
+#include "bullet.h"
+
+
+BulletTransformHelper::BulletTransformHelper(Bullet* bullet, Hardpoint* hardpoint, float bulletSpeed, const glm::vec3& target):
+    m_bullet(bullet),
+    m_hardpoint(hardpoint),
+    m_bulletSpeed(bulletSpeed),
+    m_target(target)
+{
+
+}
+
+void BulletTransformHelper::transform() {
+    WorldTransform bulletTransform(m_bullet->transform());
+
+    glm::quat shipOrientation = m_hardpoint->ship()->transform().orientation();
+    glm::vec3 bulletDirection = glm::normalize(m_target - m_hardpoint->position());
+    glm::vec3 hardpointDirection = shipOrientation * glm::vec3(0, 0, -1);
+    glm::vec3 bulletUp = glm::cross(bulletDirection, hardpointDirection);
+
+    bulletTransform.setOrientation(shipOrientation);
+
+/*
+    Below code leads to nan() - orientation of bullets sometimes. Disabled until someone unmagics and debugs it.
+    I have my guesses, yet no idea what it does.
+    No major visual loss...
+*/
+//    if (bulletUp != glm::vec3(0)) {
+//        glm::vec3 rotationAxis = glm::normalize(cross);
+//        float angle = glm::acos(glm::dot(bulletDirection, hardpointDirection));
+//        bulletTransform.rotateWorld(glm::angleAxis(-glm::degrees(angle), rotationAxis)); //then rotate towards target
+//    }
+
+    float bulletLength = m_bullet->minimalGridAABB().extent(ZAxis) * m_bullet->transform().scale();
+    bulletTransform.setPosition(m_hardpoint->position() + bulletDirection * (bulletLength / 2.0f));
+
+    m_bullet->setTransform(bulletTransform);
+    m_bullet->physics().setSpeed(bulletDirection * m_bulletSpeed);
+}
+
+
