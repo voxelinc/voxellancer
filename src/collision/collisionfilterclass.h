c9d91bf2b389787e0c9cd7c68bafc86a8b8256fe
Christian Dullweber <xchrdw@gmail.com>
2014-01-11 18:10:54 +0100

    remove explosionparticle from enum

--- src/collision/collisionfilterclass.h
+++ src/collision/collisionfilterclass.h
@@ -5,8 +5,7 @@ enum class CollisionFilterClass {
     Bullet              = 1 << 0,
     Rocket              = 1 << 1,
     Ship                = 1 << 2,
-    ExplosionParticle   = 1 << 3,
-    Other               = 1 << 4
+    Other               = 1 << 3
 };
 
 
