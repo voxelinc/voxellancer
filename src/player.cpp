d23c4dda80df029b1081b4be50d910148394fe90
gersseba <sebastian.gerstenberg@student.hpi.uni-potsdam.de>
2014-01-13 20:28:19 +0100

    coding convention

--- src/player.cpp
+++ src/player.cpp
@@ -1,30 +1,31 @@
 #include "player.h"
 
-Player::Player(Camera* camera){
+Player::Player(Camera* camera) {
     m_camera = camera;
     m_shipOffset = glm::vec3(0, 5, 10);
     acceleration = glm::vec3(0);
     accelerationAngular = glm::vec3(0);
 }
 
-void Player::move(glm::vec3 direction){
+void Player::move(glm::vec3 direction) {
     acceleration += direction;
 }
 
-void Player::rotate(glm::vec3 direction){
+void Player::rotate(glm::vec3 direction) {
     accelerationAngular += direction;
 }
 
-void Player::setShip(Ship* ship){
+void Player::setShip(Ship* ship) {
     m_playerShip = ship;
     setFollowCam();
 }
 
-void Player::applyUpdate(){
-    if (acceleration != glm::vec3(0))
+void Player::applyUpdate() {
+    if (acceleration != glm::vec3(0)) {
         acceleration = glm::normalize(acceleration);
+    }
     m_playerShip->accelerate(acceleration);
-    if (accelerationAngular == glm::vec3(0)){ // dampen rotation
+    if (accelerationAngular == glm::vec3(0)) { // dampen rotation
         accelerationAngular = m_playerShip->physics().angularSpeed();
         accelerationAngular *= -1.5f;
     }
@@ -33,19 +34,19 @@ void Player::applyUpdate(){
     accelerationAngular = glm::vec3(0);
 }
 
-void Player::setFollowCam(){
+void Player::setFollowCam() {
     m_camera->setPosition(glm::mix(m_camera->position(), m_playerShip->transform().position(), 0.5f));
     m_camera->setPosition(m_playerShip->transform().position());
     m_camera->setOrientation(glm::slerp(m_camera->orientation(), m_playerShip->transform().orientation(), 0.5f));
-    m_camera->move(glm::vec3(0,m_playerShip->transform().center().y+10,m_playerShip->transform().center().z+15));
+    m_camera->move(glm::vec3(0, m_playerShip->transform().center().y + 10, m_playerShip->transform().center().z + 15));
     m_camera->rotateX(-5);
 }
 
-Ship* Player::playerShip(){
+Ship* Player::playerShip() {
     return m_playerShip;
 }
 
-void Player::setShipToCam(float delta_sec){
+void Player::setShipToCam(float delta_sec) {
     acceleration *= 2;
     accelerationAngular /= 30;
     m_camera->move(acceleration);
@@ -65,6 +66,6 @@ void Player::setShipToCam(float delta_sec){
     accelerationAngular = glm::vec3(0);
 }
 
-void Player::boost(){
+void Player::boost() {
 
 }
