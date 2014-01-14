094be908a40725442716cb4e13a17fbd191e6c05
mrzzzrm <mrzesn@t-online.de>
2014-01-14 21:52:21 +0100

    covering basic coding-conventions

---
+++ doc/styleguide.cpp
@@ -0,0 +1,48 @@
+#include "styleguide.h"
+
+
+// After two newlines...
+StyleGuide::StyleGuide():
+    m_somePrivateMember(1337)
+{ // { in newline if there the stuff above doesn't fit into one line, applies to every method, not only constructors
+
+}
+
+StyleGuide::~StyleGuide() {
+
+}
+
+const Value& StyleGuide::value() const {
+    return m_value;
+}
+
+void StyleGuide::setValue(const Value &value) {
+    m_value = value;
+}
+
+void StyleGuide::update() {
+    if (m_worldIsSaved) { // Always enclose blocks with {}, even if they are only 1 line long
+        saveWorld();
+    }
+
+    for (int x = 0; x < 10; x++) {
+        x += 1;
+        foo();
+    }
+
+    switch (seats) {
+    case Taken:
+    break;
+
+    default:
+        takeSeat();
+    }
+
+    while (true) {
+        rofl();
+    }
+}
+
+
+
+
--- doc/styleguide.h
+++ doc/styleguide.h
@@ -20,24 +20,48 @@
 #include "anotherstyleguide.h"
 
 
-// After 2 newlines, forward-declarations
-class YetAnotherStyleguide;
+/*
+After 2 newlines, forward-declarations
+Use forward-declarations as often as possible
+because they can speed up compilation
+*/
+class SomeForwardDeclarations;
 
 class StyleGuide : public A, public B, private C
 {
 public:
-    StyleGuide()
+    StyleGuide();
+    /*
+        Declare destructors only if they are needed, make them virtual only if the class contains other virtual
+        members
+    */
     virtual ~StyleGuide();
-    
-    const Value &value() const;
-    void setValue(const Value &value) const;
-    
-// 2 newlines after last public member  
+
+    const Value& value() const;
+    void setValue(const Value &value);
+
+    /*
+    Declare every method that overrides a virtual method from the base-class
+    as virtual and, very important, override
+    */
+    virtual void update() override;
+
+    /*
+        For member-functions, try to keep these general rules
+        -> Order: Constructors -> Destructor -> Getters/Setters -> other operations
+        -> Group functions (i.e.: don't put newlines between them) if they are getter
+            and setter for the same member or are otherwise related (addVoxel and removeVoxel, e.g.)
+    */
+
+// 2 newlines after last public member
 protected:
 
 
-// 2 newlines after last protected member  
-// Only make private if really necessary 
+// 2 newlines after last protected member
+// Only make private if really necessary
 private:
-    
+    Foo m_somePrivateMember;
+
+    void somePrivateFunction();
 };
+
