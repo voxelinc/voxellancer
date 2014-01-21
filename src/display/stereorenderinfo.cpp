ee0e1293fd5018de4a00aa3bb23cee13a3770e96
mrzzzrm <mrzesn@t-online.de>
2014-01-21 23:43:19 +0100

    added z offset for eye to make head-turns more realistic in hmd-mode. cc-adjustments for property-system

--- src/display/stereorenderinfo.cpp
+++ src/display/stereorenderinfo.cpp
@@ -11,7 +11,7 @@
 #include "utils/metrics.h"
 
 
-StereoRenderInfo StereoRenderInfo::fromOVRInfo(const OVR::HMDInfo& hmdInfo) { std::cout << "From OVR" << std::endl;
+StereoRenderInfo StereoRenderInfo::fromOVRInfo(const OVR::HMDInfo& hmdInfo) {
     StereoRenderInfo result;
 
     result.m_hScreenSize = hmdInfo.HScreenSize;
@@ -82,7 +82,8 @@ StereoRenderInfo::StereoRenderInfo():
     m_hResolution(0),
     m_vResolution(0),
     m_distortionScale(0.0f),
-    m_fovy(0.0f)
+    m_fovy(0.0f),
+    m_eyeZOffset("vr.eyeZOffset")
 {
 
 }
@@ -145,11 +146,11 @@ glm::vec2 StereoRenderInfo::rightEyeLensCenter() const {
 }
 
 glm::vec3 StereoRenderInfo::leftEyeOffset() const {
-    return glm::vec3(Metrics::instance()->toGameUnits(-m_interpupillaryDistance / 2.0f), 0.0f, 0.0f);
+    return glm::vec3(Metrics::instance()->toGameUnits(-m_interpupillaryDistance / 2.0f), 0.0f, Metrics::instance()->toGameUnits(m_eyeZOffset.get()));
 }
 
 glm::vec3 StereoRenderInfo::rightEyeOffset() const {
-    return glm::vec3(Metrics::instance()->toGameUnits(m_interpupillaryDistance / 2.0f), 0.0f, 0.0f);
+    return glm::vec3(Metrics::instance()->toGameUnits(m_interpupillaryDistance / 2.0f), 0.0f, Metrics::instance()->toGameUnits(m_eyeZOffset.get()));
 }
 
 glm::vec3 StereoRenderInfo::leftEyeProjectionOffset() const {
--- src/property/propertymanager.inl
+++ src/property/propertymanager.inl
@@ -3,18 +3,17 @@
 #include "propertycollection.h"
 
 template <class T>
-void PropertyManager::registerProperty(Property<T> * prop)
-{
+void PropertyManager::registerProperty(Property<T>* prop) {
     getPropertyCollection(prop)->registerProperty(prop);
 }
 
 template <class T>
-void PropertyManager::unregisterProperty(Property<T> * prop)
-{
+void PropertyManager::unregisterProperty(Property<T>* prop) {
     getPropertyCollection(prop)->unregisterProperty(prop);
 }
 
 template <class T>
-T PropertyManager::get(char * name) {
+T PropertyManager::get(const char* name) {
     return getPropertyCollection<T>(nullptr)->get(name);
 }
+
