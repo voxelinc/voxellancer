0a5adff1af3cc612cec3dc903b2716eae8a63db9
Christian Dullweber <xchrdw@gmail.com>
2014-02-04 16:01:17 +0100

    fix splitdetector didn't work for playership (or any collision between ships)

--- src/world/handler/splitdetector.cpp
+++ src/world/handler/splitdetector.cpp
@@ -124,10 +124,10 @@ void SplitDetector::visit(const glm::ivec3& p) {
 
 
 void SplitDetector::createSplitData(WorldObject* worldObject) {
-    m_splitDataList.resize(m_nextGroupId);
+    std::vector<SplitData*> splitDataList;
     for (int i = 0; i < m_nextGroupId; i++)
     {
-        m_splitDataList[i] = new SplitData(worldObject);
+        splitDataList.push_back(new SplitData(worldObject));
     }
 
     int crucialVoxelGroup = 0;
@@ -140,22 +140,26 @@ void SplitDetector::createSplitData(WorldObject* worldObject) {
     {
         if (v.voxel && v.groupId != crucialVoxelGroup) {
             assert(v.groupId != UNKNOWN);
-            m_splitDataList[v.groupId]->addVoxel(v.voxel);
+            splitDataList[v.groupId]->addVoxel(v.voxel);
         }
     }
 
     if (crucialVoxelGroup > 0) {
-        m_splitDataList.erase(m_splitDataList.begin() + crucialVoxelGroup);
+        splitDataList.erase(splitDataList.begin() + crucialVoxelGroup);
     } else {
         int biggestIndex = 0;
-        int biggestSize = m_splitDataList[0]->splitOffVoxels().size();
-        for (int i = 0; i < m_splitDataList.size(); i++) {
-            if (m_splitDataList[i]->splitOffVoxels().size() > biggestSize) {
+        int biggestSize = splitDataList[0]->splitOffVoxels().size();
+        for (int i = 0; i < splitDataList.size(); i++) {
+            if (splitDataList[i]->splitOffVoxels().size() > biggestSize) {
                 int biggestIndex = i;
-                int biggestSize = m_splitDataList[i]->splitOffVoxels().size();
+                int biggestSize = splitDataList[i]->splitOffVoxels().size();
             }
         }
-        m_splitDataList.erase(m_splitDataList.begin() + biggestIndex);
+        splitDataList.erase(splitDataList.begin() + biggestIndex);
+    }
+    
+    for (SplitData* splitData : splitDataList) {
+        m_splitDataList.push_back(splitData);
     }
 }
 
