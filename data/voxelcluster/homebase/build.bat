bbfea54fb246dbc9b503bf89b06841b42d51c25f
psieg <padi@padimail.de>
2014-03-07 00:00:55 +0100

    wip

--- data/voxelcluster/homebase/build.bat
+++ data/voxelcluster/homebase/build.bat
@@ -9,6 +9,24 @@ python ../../../tools/csvmergez.py _bf2.csv _b2.csv _back.csv
 
 
 python ../../../tools/csvmergex.py mid.csv empty.csv _m1.csv
-python ../../../tools/csvmergex.py _m1.csv empty.csv _m2.csv
+python ../../../tools/csvmergex.py _m1.csv empty.csv _mid.csv
 
-python ../../../tools/csvmergez.py _m2.csv _back.csv _out.csv
+
+
+python ../../../tools/csvmergex.py frontbackward.csv empty.csv _fb1.csv
+python ../../../tools/csvmergex.py _fb1.csv empty.csv _fb2.csv
+
+python ../../../tools/csvmergex.py frontcenter.csv empty.csv _f1.csv
+python ../../../tools/csvmergex.py _f1.csv empty.csv _f2.csv
+
+python ../../../tools/csvmergex.py frontforward.csv empty.csv _ff1.csv
+python ../../../tools/csvmergex.py _ff1.csv empty.csv _ff2.csv
+
+python ../../../tools/csvmergez.py _f2.csv _fb2.csv _fr1.csv
+python ../../../tools/csvmergez.py _ff2.csv _fr1.csv _front.csv
+
+
+
+python ../../../tools/csvmergez.py _mid.csv _back.csv _rear.csv
+python ../../../tools/csvmergez.py _front.csv _rear.csv _right.csv
+python ../../../tools/csvmirrorx.py _right.csv _out.csv
