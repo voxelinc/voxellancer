b7f99b3a0fa5b42b7aff91732290ec65f1eb5453
Moritz Eyssen <moritz.eyssen@student.hpi.uni-potsdam.de>
2014-04-23 19:02:18 +0200

    retrieve quaternion from view-matrix

--- src/camera/camera.h
+++ src/camera/camera.h
@@ -13,12 +13,6 @@ class Camera : public glowutils::Camera {
 public:
     Camera(int viewportWidth, int viewportHeight);
 
-    /**
-     * Can't be set, orientation()/setOrientation() does their job.
-     */
-    void setCenter(const glm::vec3 & center) = delete;
-    void setUp(const glm::vec3 & up) = delete;
-
     glm::quat orientation() const;
     void setOrientation(const glm::quat& orientation);
 
