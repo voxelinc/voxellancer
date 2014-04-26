19baa984ed6fe9463c9488a1087b7bc53b7bc3af
mrzzzrm <mrzesn@t-online.de>
2014-04-26 13:47:38 +0200

    intermediate

--- src/world/handler/damageforwarder.cpp
+++ src/world/handler/damageforwarder.cpp
@@ -18,52 +18,8 @@
 void DamageForwarder::forwardDamageImpacts(std::list<DamageImpact> &dampedDeadlyDamageImpacts) {
     m_damageImpactAccumulator.clear();
 
-    for(DamageImpact &dampedDeadlyDamageImpact : dampedDeadlyDamageImpacts) {
-        Voxel *deadVoxel = dampedDeadlyDamageImpact.voxel();
-
-        m_currentWorldObject = dampedDeadlyDamageImpact.worldObject();
-        VoxelNeighbourHelper nHelper(m_currentWorldObject, true);
-        const std::vector<Voxel*> neighbours = nHelper.neighbours(deadVoxel);
-
-
-        std::list<std::pair<Voxel*, glm::vec3>> forwardedPerNeighbour;
-        float totalForwardedDamage = 0.0f;
-
-        for(Voxel* neighbour : neighbours) {
-            glm::vec3 voxelVec = glm::normalize(static_cast<glm::vec3>(neighbour->gridCell() - deadVoxel->gridCell()));
-            glm::vec3 damageImpactVec = glm::normalize(glm::inverse(m_currentWorldObject->transform().orientation()) * dampedDeadlyDamageImpact.damageVec());
-
-            // diagonals get less damage
-            float distanceFactor = 1.0f;
-            if (voxelVec.x + voxelVec.y + voxelVec.z > 1.0f) {
-                distanceFactor = 0.7f;
-            }
-
-            float dotProduct = glm::dot(damageImpactVec, voxelVec);
-            glm::vec3 forwardedDamage = dampedDeadlyDamageImpact.damageVec() * forwardFactor(dotProduct, dampedDeadlyDamageImpact.fieldOfDamage(), neighbours.size());
-            glm::vec3 createdDamage = voxelVec * deadVoxel->damageForwardingDestructionDamage();
-
-            glm::vec3 actualForwardedDamage = distanceFactor * (forwardedDamage + createdDamage);
-            totalForwardedDamage += glm::length(actualForwardedDamage);
-
-            forwardedPerNeighbour.push_back(std::make_pair(neighbour, actualForwardedDamage));
-        }
-
-        if (forwardedPerNeighbour.empty() || totalForwardedDamage == 0.0f) {
-            continue;
-        }
-
-        // Hacked fix to prevent damage from being lost during forwarding
-        float losslessFactor = (dampedDeadlyDamageImpact.damage() / totalForwardedDamage) * (forwardedPerNeighbour.size() / 26.0f);
-
-        for (auto& pair : forwardedPerNeighbour) {
-            DamageImpact forwarded( m_currentWorldObject,
-                                    pair.first,
-                                    pair.second * losslessFactor,
-                                    dampedDeadlyDamageImpact.fieldOfDamage());
-            m_damageImpactAccumulator.parse(forwarded);
-        }
-
+    for(DamageImpact& damageImpact : dampedDeadlyDamageImpacts) {
+        forward(damageImpact);
     }
 }
 
@@ -75,7 +31,58 @@ std::list<DamageImpact> DamageForwarder::forwardedDamageImpacts() {
     return m_damageImpactAccumulator.accumulatables();
 }
 
+void DamageForwarder::forward(DamageImpact& damageImpact) {
+    m_currentDeadVoxel = damageImpact.voxel();
+    m_currentWorldObject = damageImpact.worldObject();
+    m_currentDamageVec = damageImpact.damageVec();
+    m_currentFieldOfDamage = damageImpact.fieldOfDamage();
+
+    std::vector<Voxel*> neighbours = VoxelNeighbourHelper(m_currentWorldObject, true).neighbours(deadVoxel);
+
+    std::list<std::pair<Voxel*, glm::vec3>> forwardedPerNeighbour;
+    float totalForwardedDamage = 0.0f;
+
+    for(Voxel* neighbour : neighbours) {
+        glm::vec3 damageVec = calculateForwardingToVoxel(neighbour)
+
+        totalForwardedDamage += glm::length(damageVec);
+
+        forwardedPerNeighbour.push_back(std::make_pair(neighbour, damageVec));
+    }
+
+    if (forwardedPerNeighbour.empty() || totalForwardedDamage == 0.0f) {
+        continue;
+    }
+
+    // Hacked fix to prevent damage from being lost during forwarding
+    float losslessFactor = (dampedDeadlyDamageImpact.damage() / totalForwardedDamage) * (forwardedPerNeighbour.size() / 26.0f);
+
+    for (auto& pair : forwardedPerNeighbour) {
+        DamageImpact forwarded( m_currentWorldObject,
+                                pair.first,
+                                pair.second * losslessFactor,
+                                dampedDeadlyDamageImpact.fieldOfDamage());
+        m_damageImpactAccumulator.parse(forwarded);
+    }
+}
+
 float DamageForwarder::forwardFactor(float dotProduct, float fieldOfDamage, int neighbours) {
     return glm::max(0.0f, (fieldOfDamage - std::acos(dotProduct)) / fieldOfDamage) / neighbours;
 }
 
+glm::vec3 DamageForwarder::calculateForwardingToVoxel(Voxel* voxel) {
+    glm::vec3 voxelVec = glm::normalize(static_cast<glm::vec3>(voxel->gridCell() - m_currentDeadVoxel->gridCell()));
+    glm::vec3 damageImpactVec = glm::normalize(glm::inverse(m_currentWorldObject->transform().orientation()) * m_currentDamageVec);
+
+    // diagonals get less damage
+    float distanceFactor = 1.0f;
+    if (voxelVec.x + voxelVec.y + voxelVec.z > 1.0f) {
+        distanceFactor = 0.71f;
+    }
+
+    float dotProduct = glm::dot(damageImpactVec, voxelVec);
+    glm::vec3 forwardedDamage = m_currentDamageVec * forwardFactor(dotProduct, m_currentFieldOfDamage, neighbours.size());
+    glm::vec3 createdDamage = voxelVec * deadVoxel->damageForwardingDestructionDamage();
+
+    return distanceFactor * (forwardedDamage + createdDamage);
+}
--- src/world/handler/damageforwarder.h
+++ src/world/handler/damageforwarder.h
@@ -16,13 +16,15 @@ public:
     std::list<DamageImpact> forwardedDamageImpacts();
 
 
-protected:
-    WorldObject *m_currentWorldObject;
-    std::list<Voxel*> *m_currentNeighbours;
-    glm::ivec3 m_currentGridCell;
+protected:;
+    WorldObject* m_currentWorldObject;
+    Voxel* m_currentDeadVoxel;
+    glm::vec3 m_currentDamageVec;
+    float m_currentFieldOfDamage;
 
     VoxelAccumulator<DamageImpact> m_damageImpactAccumulator;
 
+    void forward(DamageImpact& damageImpact);
     float forwardFactor(float dotProduct, float fieldOfDamage, int neighbours);
 };
 
