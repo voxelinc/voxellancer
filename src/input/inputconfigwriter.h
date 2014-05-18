5611f7840643371b99e40f3ad2fef90d24dba581
mrzzzrm <mrzesn@t-online.de>
2014-05-18 21:21:44 +0200

    removed comment-warning in inputconfigwriter; removed streamredirect

--- src/input/inputconfigwriter.h
+++ src/input/inputconfigwriter.h
@@ -7,18 +7,17 @@
 class ActionKeyMapping;
 class InputMapping;
 
-// This will probably not work if voxellancer is installed!
-// We need to switch to a non write-protected folder for configs
 class InputConfigWriter {
-
 public:
     InputConfigWriter(const std::string& file);
     ~InputConfigWriter();
 
     void write(ActionKeyMapping& mapping);
 
+
 protected:
     std::ofstream m_file;
 
     void write(const std::string& name, const InputMapping& mapping);
 };
+
